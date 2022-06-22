cb35af8a078c71128ea51ec30f807e7dec07e116
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-06-22 21:08:45 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,27 +1,38 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-    staticruntime "On"
+	staticruntime "On"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	systemversion "latest"
 
 	files {
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",
 	}
 
 	filter "system:linux"
 		pic "On"
-
-		systemversion "latest"
 
 		files {
 			"src/x11_init.c",
@@ -41,8 +52,6 @@ project "GLFW"
 		}
 
 	filter "system:windows"
-		systemversion "latest"
-
 		files {
 			"src/win32_init.c",
 			"src/win32_joystick.c",
