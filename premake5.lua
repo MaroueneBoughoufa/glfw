2c2b9bcda3671854e3ee575a251424f752d40815
Marouène Boughoufa <maroueneboughoufa@gmail.com>
2022-07-01 17:49:01 +0100

    Reverted to previous commits due to not being able to compile

--- premake5.lua
+++ premake5.lua
@@ -1,39 +1,27 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "On"
+    staticruntime "On"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-	
-	systemversion "latest"
 
 	files {
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/internal.h",
-		"src/platform.h",
-		"src/mappings.h",
+		"src/glfw_config.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
-		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c",
-		"src/egl_context.c",
-		"src/osmesa_context.c",
-		"src/null_platform.h",
-		"src/null_joystick.h",
-		"src/null_init.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-		"src/null_joystick.c",
+		"src/window.c"
 	}
 
 	filter "system:linux"
-		linkgroups "On"
 		pic "On"
+
+		systemversion "latest"
 
 		files {
 			"src/x11_init.c",
@@ -52,15 +40,9 @@ project "GLFW"
 			"_GLFW_X11"
 		}
 
-		links {
-			"Xrandr",
-			"Xi",
-			"GL",
-			"X11",
-			"dl"
-		}
-
 	filter "system:windows"
+		systemversion "latest"
+
 		files {
 			"src/win32_init.c",
 			"src/win32_joystick.c",
